1bdee910cdaab21c2746543201bef91faa32c064
ANKIT KUMAR <ankityadav12044@gmail.com>
2025-02-18 20:04:33 +0530

    ankit ydav commit

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -7,12 +7,12 @@ function App() {
       {/* Footer */}
       <footer className="footer">
         <div className="social-links">
-          <a href="#"><i className="fa-brands fa-linkedin-in"></i></a>
-          <a href="#"><i className="fa-brands fa-github"></i></a>
-          <a href="#"><i className="fa-brands fa-instagram"></i></a>
-          <a href="#"><i className="fa-brands fa-facebook"></i></a>
-          <a href="#"><i className="fa-brands fa-whatsapp"></i></a>
-        </div>
+           <a href="https://www.linkedin.com/in/ankit-kumar-889aa6288/" ><i className="fa-brands fa-linkedin-in"></i></a>
+                     <a href="https://github.com/ankitkumariuj/"><i className="fa-brands fa-github"></i></a>
+                     <a href="https://www.instagram.com/ankit_yadav_1204/"><i className="fa-brands fa-instagram"></i></a>
+                     <a href="https://www.facebook.com/people/Ankit-Kumar/pfbid0eThjyTeF5ooXbbbujDB9cm5o7NBvFuthp3z1Lg6R6npgXy77bvz3FMyuFjipcU3Ml/"><i className="fa-brands fa-facebook"></i></a>
+                     <a href="https://wa.me/7272875008?text=Hello%20I%20need%20support"><i className="fa-brands fa-whatsapp"></i></a>
+                  </div>
         <p>© 2025 Ankit Kumar. All Rights Reserved.</p>
       </footer>
     </div>
--- src/Hero.jsx
+++ src/Hero.jsx
@@ -45,7 +45,7 @@ function App() {
           transition={{ duration: 2, delay: 0.5 }}
         >
           <div className="link-icon" >
-            <a href="https://www.linkedin.com/in/ankit-kumar-889aa6288/" ><i className="fa-brands fa-linkedin-in"></i></a>
+          <a href="https://www.linkedin.com/in/ankit-kumar-889aa6288/" ><i className="fa-brands fa-linkedin-in"></i></a>
             <a href="https://github.com/ankitkumariuj/"><i className="fa-brands fa-github"></i></a>
             <a href="https://www.instagram.com/ankit_yadav_1204/"><i className="fa-brands fa-instagram"></i></a>
             <a href="https://www.facebook.com/people/Ankit-Kumar/pfbid0eThjyTeF5ooXbbbujDB9cm5o7NBvFuthp3z1Lg6R6npgXy77bvz3FMyuFjipcU3Ml/"><i className="fa-brands fa-facebook"></i></a>

514560bf12b7f72294876ac3623e5fae963c8dec
ANKIT KUMAR <ankityadav12044@gmail.com>
2025-02-18 19:50:59 +0530

    ankit commit

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -45,11 +45,11 @@ function App() {
           transition={{ duration: 2, delay: 0.5 }}
         >
           <div className="link-icon" >
-            <a href="#" ><i className="fa-brands fa-linkedin-in"></i></a>
-            <a href="#"><i className="fa-brands fa-github"></i></a>
-            <a href="#"><i className="fa-brands fa-instagram"></i></a>
-            <a href="#"><i className="fa-brands fa-facebook"></i></a>
-            <a href="#"><i className="fa-brands fa-whatsapp"></i></a>
+            <a href="https://www.linkedin.com/in/ankit-kumar-889aa6288/" ><i className="fa-brands fa-linkedin-in"></i></a>
+            <a href="https://github.com/ankitkumariuj/"><i className="fa-brands fa-github"></i></a>
+            <a href="https://www.instagram.com/ankit_yadav_1204/"><i className="fa-brands fa-instagram"></i></a>
+            <a href="https://www.facebook.com/people/Ankit-Kumar/pfbid0eThjyTeF5ooXbbbujDB9cm5o7NBvFuthp3z1Lg6R6npgXy77bvz3FMyuFjipcU3Ml/"><i className="fa-brands fa-facebook"></i></a>
+            <a href="https://wa.me/7272875008?text=Hello%20I%20need%20support"><i className="fa-brands fa-whatsapp"></i></a>
           </div>
         </motion.div>
       </motion.div>

43d02d19f01b52c9e63d9e1f3727bcadf5b1674e
ANKIT KUMAR <ankityadav12044@gmail.com>
2025-02-18 20:17:16 +0530

    ankit first  ydav commit

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -11,7 +11,7 @@ function App() {
                      <a href="https://github.com/ankitkumariuj/"><i className="fa-brands fa-github"></i></a>
                      <a href="https://www.instagram.com/ankit_yadav_1204/"><i className="fa-brands fa-instagram"></i></a>
                      <a href="https://www.facebook.com/people/Ankit-Kumar/pfbid0eThjyTeF5ooXbbbujDB9cm5o7NBvFuthp3z1Lg6R6npgXy77bvz3FMyuFjipcU3Ml/"><i className="fa-brands fa-facebook"></i></a>
-                     <a href="https://wa.me/7272875008?text=Hello%20I%20need%20support"><i className="fa-brands fa-whatsapp"></i></a>
+                     <a href="https://wa.me/7292875008?text=Hello%20I%20need%20support"><i className="fa-brands fa-whatsapp"></i></a>
                   </div>
         <p>© 2025 Ankit Kumar. All Rights Reserved.</p>
       </footer>
